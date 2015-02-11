56c9ac0002ae669758f2f6031fe18f7c7db568d4
kirk7880@gmail.com <kirk7880@gmail.com>
2015-02-11 12:39:28 -0500

    Initial commit

--- javascript/algorithms/permutations/string.js
+++ javascript/algorithms/permutations/string.js
@@ -12,21 +12,21 @@ function permutations(perm, used, data) {
     // add current to used stack
     used.push(current);
 
-
     if (data.length === 0) {
       perm.push(used.slice());
     }
 
+    // recursively permutate the
+    // data...
     permutations(perm, used, data);
 
+    // replace char at i with "current"
     data.splice(i, 0, current);
-    
+
     used.pop();
   }
 
   return perm;
 }
-
-
 
 console.log(p([], [], ["a", "b", "c", "d"]));
