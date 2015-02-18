32056b98c384d79c9c6ac774c6457600cefc5b7e
kirk7880@gmail.com <kirk7880@gmail.com>
2015-02-18 14:15:46 -0500

    Initial commit of BinaryHeap

---
+++ javascript/datastructures/BinaryHeap/index.js
@@ -0,0 +1,233 @@
+/** 
+Implementation of a binary heap
+-- Binary Tree (Structure)
+-- Heap property (Parent priority <= to child nodes)
+*/
+var BinaryHeap = function(comparator) {
+  this.heap = [];
+  this.comparator = comparator;
+  this.length = 0;
+};
+
+BinaryHeap.prototype.size = function() {
+  return this.length;
+};
+
+/**
+Returns the left child index of parent n
+
+@param {Number} idx Parent Index
+@return {Number} index of the child
+*/
+BinaryHeap.prototype.getLeftChildIndex = function(idx) {
+  return Math.floor((idx << 1));
+};
+
+/**
+Returns the left child element at position n
+
+@param {Number} idx Index of element to return
+@return {Object} Node element
+*/
+BinaryHeap.prototype.getLeftChild = function(idx) {
+  return this.heap[this.getLeftChildIndex(idx)];
+};
+
+/**
+Returns the right child index of parent n
+
+@param {Number} idx Index of parent element
+@return {Number} The index of the child element
+*/
+BinaryHeap.prototype.getRightChildIndex = function(idx) {
+  return Math.floor((idx << 1) + 1);
+}
+
+/**
+Returns the right child element at position n
+
+@param {Number} idx The index of the child element
+@param {Object} Node element
+*/
+BinaryHeap.prototype.getRightChild = function(idx) {
+  return this.heap[this.getRightChildIndex(idx)];
+};
+
+/**
+Returns the parent index for element at position n
+
+@param {Number} idx Index of child element
+@return {Number} Index of parent element
+*/
+BinaryHeap.prototype.getParentIndex = function(idx) {
+  return Math.floor((idx >> 1));
+};
+
+BinaryHeap.prototype.getParent = function(idx) {
+  return this.heap[this.getParentIndex(idx)];
+};
+
+BinaryHeap.prototype.enqueue = function(item) {
+  this.heap[(this.length++)] = item;
+  this.bubbleUp(this.length - 1);
+
+  return this;
+};
+
+/**
+Removes the given node from the queue
+
+@param {Object} item Item to remove
+@return {Object} this
+*/
+BinaryHeap.prototype.remove = function(item) {
+  if (typeof item === 'undefined') {
+    return null;
+  }
+
+  var len = this.length;
+  for (var i=0; i<len; i++) {
+    if (this.heap[i] === node) {
+      var end = this.heap.pop();
+      this.heap.slice(i, 1);
+      this.length--;
+      this.heap[i] = end;
+      this.bubbleUp(i);
+      this.sink(i);
+      break;
+    }
+  }
+
+  return this;
+};
+
+BinaryHeap.prototype.bubbleUp = function(idx) {
+  var item = this.heap[idx];
+  var score = this.comparator(item);
+  while (idx > 0) {
+    // get the parent of the current element
+    var parent = this.getParent(idx);
+    var parentIdx = this.getParentIndex(idx);
+
+    if (score >= this.comparator(parent)) {
+      break;
+    }
+
+    this.heap[parentIdx] = item;
+    this.heap[idx] = parent;
+    idx = parentIdx;
+  }
+
+  return this;
+};
+
+BinaryHeap.prototype.dequeue = function() {
+  var item = this.heap[0];
+  var end = this.heap.pop();
+  this.length -= 1;
+
+  if (this.length > 0) {
+    this.heap[0] = end;
+    this.sink(0);
+  }
+
+  return item;
+};
+
+BinaryHeap.prototype.sink = function(idx) {
+  var len = this.length;
+  var item = this.heap[idx];
+  var score = this.comparator(item);
+  var leftChild = null;
+  var rightChild = null;
+  var leftChildScore = NaN;
+  var rightChildScore = NaN;
+  var swap = null;
+  var left = 0;
+  var rigth = 0;
+
+  while(true) {
+    // reinitialize to null
+    swap = null;
+
+    // get child indexes
+    right = this.getRightChildIndex(idx); 
+    left = this.getLeftChildIndex(idx);
+
+    // reinitialize scores to NaN
+    rightChildScore = NaN;
+    leftChildScore = NaN;
+
+    if (left < len) {
+      leftChild = this.heap[left];
+      leftChildScore = this.comparator(leftChild);
+
+      // swap the item with the left child
+      if (leftChildScore < score) {
+        swap = left;
+      }
+    }
+
+    if (right < len) {
+      rightChild = this.heap[right];
+      rightChildScore = this.comparator(rightChild);
+
+      if (rightChildScore < (isNaN(leftChildScore) ? score : leftChildScore)) {
+        swap = right;
+      }
+    }
+
+    if (swap === null) {
+      break;
+    }
+
+    // move the swappable element into the current index position
+    this.heap[idx] = this.heap[swap];
+
+    // move the item into the swappable element's position...
+    this.heap[swap] = item;
+
+    // make the current index the swappable element's index
+    idx = swap;
+  }
+};
+
+module.exports = BinaryHeap;
+
+///// TEST ////
+var heap = new BinaryHeap(function(task) { 
+  return task.priority;
+});
+
+heap.enqueue({
+  'task': 1
+  ,priority: 10
+});
+
+heap.enqueue({
+  'task': 2
+  ,priority: 6
+});
+
+heap.enqueue({
+  'task': 3
+  ,priority: 9
+});
+
+var node = {
+  'task': 4
+  ,priority: 7
+};
+
+heap.enqueue(node);
+
+heap.remove(node);
+
+heap.enqueue(node);
+
+while(heap.size() > 0) {
+  console.log('tasks', heap.dequeue());
+};
+
+console.log('size', heap.size());
+
--- javascript/datastructures/Set/set.js
+++ javascript/datastructures/Set/set.js
@@ -8,19 +8,63 @@ var Set = function(set) {
     throw new TypeError('The provided set should be an array!');
   }
 
-  this.data = new Iterator(this.filter(set)) || new Iterator();
+  var data = new Iterator(this.filter(set)) || new Iterator();
 
   return {
-    data: this.data
-    ,add: this.add
-    ,has: this.has
-    ,clear: this.clear
-    ,diff: this.diff
-    ,union: this.union
-    ,clone: this.clone
-    ,hasNext: this.data.hasNext
-    ,next: this.data.next
-    ,size: this.data.size
+    add: function(data) {
+      if (this.has(data)) {
+        return false;
+      }
+
+      data.add(data);
+      return true;
+    }
+
+    ,has: function(item) {
+      return data.contains(item);
+    }
+
+    ,clear: function() {
+      data = new Iterator();
+    }
+
+    ,clone: function() {
+      return new Iterator(data.collection.slice());
+    }
+
+    ,diff: function(set) {
+      var len = 0;
+      var diff = [];
+      var tmp = this.clone();
+
+      while(tmp.hasNext()) {
+        var item = tmp.next();
+        if (!set.has(item)) {
+          diff[len++] = item;
+        }
+      }
+
+      return diff;
+    }
+
+    ,union: function(set) {
+      var index = 0;
+      var tmp = this.clone();
+
+      while(set.hasNext()) {
+        var item = set.next();
+        if (!tmp.has(item)) {
+          tmp.add(item);
+        } 
+      }
+
+      return tmp;
+    }
+
+    ,hasNext: data.hasNext
+    ,next: data.next
+    ,size: data.size
+    ,sort: data.collection.sort
   }
 };
 
@@ -46,95 +90,6 @@ Set.prototype.filter = function(data) {
   return tmp;
 };
 
-/**
-Add a new element to the collection
-- If the collection is an object,
-the property is added as key=>value
-
-In the case of this Set class,
-the collection is of type array
-
-@param {mixed} data 
-@return {boolean} True if the element was added
-  or false otherwise.
-*/
-Set.prototype.add = function(data) {
-  if (this.has(data)) {
-    return false;
-  }
-
-  this.data.add(data);
-  return true;
-};
-
-/**
-Check if this data element already
-exists in the collection.
-
-@param {mixed} data
-@return {boolean} True if the element exists
-  in the collection or false otherwise.
-*/
-Set.prototype.has = function(data) {
-  return this.data.contains(data);
-};
-
-Set.prototype.clear = function() {
-  this.data = new Iterator();
-};
-
-Set.prototype.hasNext = function() {
-  return this.data.hasNext()
-};
-
-Set.prototype.next = function() {
-  return this.data.next();
-};
-
-Set.prototype.clone = function() {
-  return new Set(this.data.collection.slice());
-};
-
-/**
-Returns the difference from comparing
-two Set collections.
-
-@returns {Array} Array of difference.
-*/
-Set.prototype.diff = function(set) {
-  var len = 0;
-  var diff = [];
-  var tmp = this.clone();
-
-  while(tmp.data.hasNext()) {
-    var item = tmp.data.next();
-    if (!set.has(item)) {
-      diff[len++] = item;
-    }
-  }
-
-  return diff;
-};
-
-
-/**
-Returns a new Set with distint elements
-from both collections
-*/
-Set.prototype.union = function(set) {
-  var index = 0;
-  var tmp = this.clone();
-
-  while(set.data.hasNext()) {
-    var item = set.data.next();
-    if (!tmp.has(item)) {
-      tmp.add(item);
-    } 
-  }
-
-  return tmp;
-};
-
 var s = new Set([1, 3, 4, 5]);
 var b = new Set([2, 5, 10]);
 
@@ -142,4 +97,4 @@ var union = b.union(s);
 var diff = s.diff(b);
 
 console.log('diff', diff);
-console.log('union', union.data.collection.sort());
+console.log('union', union);
