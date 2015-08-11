d10bc1cf43f4abc9582dec6c8d06123d2a2e9862
kgordon <kgordon@forensiq.com>
2015-08-11 16:17:00 -0400

    Added fibonacci generator

---
+++ javascript/algorithms/fibonacci/index.js
@@ -0,0 +1,27 @@
+var memoize = function (fn, results) {
+    var memo = [0, 1];
+
+    // closure
+    return function compute(n) {
+        var current = memo[n];
+
+        // avoid truthy test by ensuring type if a number
+        if (typeof current !== 'number') {
+            current = fn(compute, n);
+            memo[n] = results[results.length] = current;
+        }
+
+        return current;
+    };
+};
+
+var cb = function(fn, n) {
+    var results = fn(n - 1) + fn( n - 2 );
+    return results;
+};
+
+var sequence = [];
+var printSequence = memoize(cb, sequence);
+printSequence(10);
+
+console.log(sequence);
